4082f4069d27f271280a3e6c576d696118321482
Ruan Lucas <ruanlsdn@gmail.com>
2022-07-14 15:13:22 -0300

    correção orientador quando atualiza aluno

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,74 +1,81 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-module.exports = function(app) {
+module.exports = function (app) {
+  var api = {};
 
-	var api = {};
+  var model = mongoose.model("Foto");
+  var modelProf = mongoose.model("Prof");
 
-	var model = mongoose.model('Foto');
-	var modelProf = mongoose.model('Prof')
+  api.lista = function (req, res) {
+    model.find({}).then(
+      function (fotos) {
+        res.json(fotos);
+      },
+      function (error) {
+        console.log(error);
+        res.sendStatus(500);
+      }
+    );
+  };
 
-	api.lista = function(req, res) {
-		model.find({})
-		.then(function(fotos) {
-			res.json(fotos); 
-		}, function(error) {
-			console.log(error);
-			res.sendStatus(500);
-		});
-	};
+  api.buscaPorId = function (req, res) {
+    model.findById(req.params.id).then(
+      function (foto) {
+        if (!foto) throw new Error("Foto não encontrada");
+        res.json(foto);
+      },
+      function (error) {
+        console.log(error);
+        res.sendStatus(500);
+      }
+    );
+  };
 
-	api.buscaPorId = function(req, res) {
+  api.removePorId = function (req, res) {
+    model.remove({ _id: req.params.id }).then(
+      function () {
+        res.sendStatus(200);
+      },
+      function (error) {
+        console.log(error);
+        res.sendStatus(500);
+      }
+    );
+  };
 
-		model.findById(req.params.id)
-		.then(function(foto) {
-			if (!foto) throw new Error('Foto não encontrada');
-			res.json(foto);
-		}, function(error) {
-			console.log(error);
-			res.sendStatus(500);
-		});
-	};
+  api.adiciona = function (req, res) {
+    const promise = async () => {
+      const response = await modelProf.findById(req.body.orientador);
+      await model.create({
+        titulo: req.body.titulo,
+        nome: req.body.nome,
+        email: req.body.email,
+        telefone: req.body.telefone,
+        url: req.body.url,
+        descricao: req.body.descricao,
+        grupo: req.body.grupo,
+        orientador: response._doc,
+      });
+    };
+    promise();
+  };
 
-	api.removePorId = function(req, res) {
+  api.atualiza = function (req, res) {
+    const promise = async () => {
+      const response = await modelProf.findById(req.body.orientador);
+      await model.findByIdAndUpdate(req.params.id, {
+        titulo: req.body.titulo,
+        nome: req.body.nome,
+        email: req.body.email,
+        telefone: req.body.telefone,
+        url: req.body.url,
+        descricao: req.body.descricao,
+        grupo: req.body.grupo,
+        orientador: response._doc,
+      });
+    };
+    promise();
+  };
 
-		model.remove({'_id' : req.params.id})
-		.then(function() {
-			res.sendStatus(200);
-		}, function(error) {
-			console.log(error);
-			res.sendStatus(500);
-		});
-
-	};
-
-	api.adiciona = function(req, res) {
-		const promise = async () =>{
-			const response = await modelProf.findById(req.body.orientador)
-			await model.create({
-				titulo: req.body.titulo,
-				nome: req.body.nome,
-				email: req.body.email,
-				telefone: req.body.telefone,
-				url: req.body.url,
-				descricao: req.body.descricao,
-				grupo: req.body.grupo,
-				orientador: response._doc,
-			})
-		}
-		promise()
-	};
-
-	api.atualiza = function(req, res) {
-
-		model.findByIdAndUpdate(req.params.id, req.body)
-		.then(function(foto) {
-			res.json(foto);
-		}, function(error) {
-			console.log(error);
-			res.sendStatus(500);
-		})
-	};
-
-	return api;
+  return api;
 };
-
