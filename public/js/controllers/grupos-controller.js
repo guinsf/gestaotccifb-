0e264cefb0d7c4705cc19d72372d21859c326ec8
guinsf <38284477+guinsf@users.noreply.github.com>
2022-08-02 14:57:57 -0300

    Comentários adicionais

--- public/js/controllers/grupos-controller.js
+++ public/js/controllers/grupos-controller.js
@@ -1,4 +1,5 @@
 angular.module('alurapic')
+/*Obtendo informações de grupos de áreas de interesses cadastrados pelo controller e sinalizando mensagem de erro caso ocorra*/
 	.controller('GruposController', function($scope, $http) {
 		$http.get('/v1/grupos')
 			.success(function(grupos) {
